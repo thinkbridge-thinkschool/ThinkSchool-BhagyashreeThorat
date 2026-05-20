5e54164a0f924007cbe868b1906e8d7a85a7292c
Bhagyashree218 <bhagyashreethorat215@gmail.com>
2026-05-20 11:52:03 +0530

    feat: add Collection aggregate with owned CollectionItem value object

---
+++ day-1/piece-7/QuotesApi/Extensions/ServiceExtensions.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using QuotesApi.Data;
+using QuotesApi.Repositories;
+
+namespace QuotesApi.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlite("Data Source=quotes.db"));
+
+        services.AddScoped<IQuoteRepository, QuoteRepository>();
+        services.AddScoped<ICollectionRepository, CollectionRepository>();
+
+        return services;
+    }
+}
---
+++ day-1/piece-7/QuotesApi/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using QuotesApi.Data;
+using QuotesApi.Extensions;
+using QuotesApi.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider
+        .GetRequiredService<AppDbContext>();
+
+    dbContext.Database.Migrate();
+}
+
+app.MapQuoteEndpoints();
+
+app.Run();
